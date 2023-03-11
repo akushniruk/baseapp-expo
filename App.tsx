3b046421b4670504fbc6b55ba9cf2eac98831854
akushniruk <akushniruk@openware.com>
2023-03-11 19:17:26 +0100

    feat: update lib to 0.0.27

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Router } from "./src/router/router";
-import { ReduxProvider } from "@akushniruk/baseapp-expo-sdk";
+import { CoreProvider } from "@akushniruk/baseapp-expo-sdk";
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
     return (
         <>
             <StatusBar style="auto" />
-            <ReduxProvider>
+            <CoreProvider>
                 <Router />
-            </ReduxProvider>
+            </CoreProvider>
         </>
     );
 }
