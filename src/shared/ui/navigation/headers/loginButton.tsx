c631fa2338a2a399398566e41af95b00cae22b2a
akushniruk <akushniruk@openware.com>
2023-05-11 00:04:52 +0200

    faet: add logic for login button on landing page

--- src/shared/ui/navigation/headers/loginButton.tsx
+++ src/shared/ui/navigation/headers/loginButton.tsx
@@ -1,19 +1,22 @@
-import { Button } from "@akushniruk/baseapp-expo-sdk";
+import { Button, useAppSelector } from "@akushniruk/baseapp-expo-sdk";
+import { RootState } from "@akushniruk/baseapp-expo-sdk/src/shared/providers/redux/model/store";
 import React, { FC, useCallback } from "react";
 import { View } from "react-native";
 
 export const LoginButton: FC<any> = ({ navigation }) => {
+    const userProfile = useAppSelector((state: RootState) => state.user.profile);
+
     const redirectToLogin = useCallback(() => {
         navigation.navigate("Login");
     }, [navigation]);
 
+    if (userProfile) {
+        return null;
+    }
+
     return (
         <View style={{ marginLeft: -10 }}>
-            <Button
-                onPress={redirectToLogin}
-                title="Login"
-                isLoading={false}
-            ></Button>
+            <Button onPress={redirectToLogin} title="Login" isLoading={false}></Button>
         </View>
     );
 };
